63de7046b1a303fc5cb0c71929dcda82ad580f4a
lass132d <lass132d@edu.campusvejle.dk>
2023-03-23 09:33:36 +0100

    added another project

---
+++ dir.c
@@ -0,0 +1,17 @@
+//need to download library
+#include <dirent.h>
+#include <stdio.h>
+
+int main(void){
+   struct dirent *files;
+   DIR *dir = opendir(".");
+   if (dir == NULL){
+      printf("Directory cannot be opened!" );
+      return 0;
+   }
+   while ((files = readdir(dir)) != NULL)
+   printf("%s
+", files->d_name);
+   closedir(dir);
+   return 0;
+}
